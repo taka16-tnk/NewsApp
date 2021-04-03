a88f32d3c67d11a9f42641a9ce9916ea3f6286fe
Takahiro Tanaka <t_tkhr@icloud.com>
2021-04-03 17:59:31 +0900

    Navigation Component Setup

--- app/src/main/java/com/demo/newsapp/MainActivity.java
+++ app/src/main/java/com/demo/newsapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.demo.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    NavHostFragment navHostFragment;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupNavigation();
+    }
+
+    private void setupNavigation() {
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
     }
 }
